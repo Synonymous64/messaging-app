fee83b59de69cc97b474430b5f3a97515ad7771e
Synonymous64 <prajwalurkude007@gmail.com>
2023-03-08 23:58:11 +0530

    messaging penal setup

--- client/src/components/StandardMessagingForms/StandardMessagingForm.jsx
+++ client/src/components/StandardMessagingForms/StandardMessagingForm.jsx
@@ -1,13 +1,39 @@
-import { PaperClipIcon, XMarkIcon } from '@heroicons/react/24/solid';
+import { PaperAirplaneIcon, PaperClipIcon, XMarkIcon } from '@heroicons/react/24/solid';
 import React from 'react'
 import { useState } from 'react';
 import Dropzone from 'react-dropzone';
-const StandardMessagingForm = () => {
+const StandardMessagingForm = ({ props, activeChat }) => {
+
+    // console.log(props);
+    // console.log(activeChat);
+
     const [message, setMessage] = useState("");
     const [attachment, setAttachment] = useState("");
     const [preview, setPreview] = useState("");
 
-    const handleChange = (e) => setMessage(e.target.value);
+    const handleChange = async (e) => setMessage(e.target.value);
+
+    const handleSubmit = async () => {
+        const date = new Date()
+            .toISOString()
+            .replace("T", " ")
+            .replace("Z", `${Math.floor(Math.random() * 1000)}+00:00`);
+
+        const at = attachment ? [{ blob: attachment, file: attachment.name }] : [];
+
+        const form = {
+            attachment: at,
+            created: date,
+            sender_username: props.username,
+            text: message,
+            activeChatId: activeChat.id,
+        };
+
+        props.onSubmit(form);
+
+        setMessage("");
+        setAttachment("");
+    };
 
     return (
         <div className='message-form-container'>
@@ -39,7 +65,7 @@ const StandardMessagingForm = () => {
                         noClick={true}
                         onDrop={(acceptedFiles) => {
                             setAttachment(acceptedFiles[0]);
-                            setPreview(URL.createObjectURL(acceptedFiles[0]))
+                            setPreview(URL.createObjectURL(acceptedFiles[0]));
                         }}
                     >
                         {({ getRootProps, getInputProps, open }) => (
@@ -52,6 +78,14 @@ const StandardMessagingForm = () => {
                             </div>
                         )}
                     </Dropzone>
+
+                    <hr className="vertical-line" />
+                    <PaperAirplaneIcon className='message-form-icon-airplane'
+                        onClick={() => {
+                            setPreview("");
+                            handleSubmit();
+                        }}
+                    />
                 </div>
             </div>
         </div>
